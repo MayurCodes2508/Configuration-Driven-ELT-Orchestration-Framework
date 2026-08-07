4d8a2bb4933c2e93cd90c64f72fac9e2e1764468
StevenCodes67 <stevenmayurreddy@gmail.com>
2026-08-07 10:13:03 +0530

    Update Cloud Run job configurations to simplify command arguments and adjust image references

--- platform/terraform/cloud_run.tf
+++ platform/terraform/cloud_run.tf
@@ -13,7 +13,7 @@ resource "google_cloud_run_v2_job" "dev_el_system_run" {
       containers {
         image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:testing"
         args = [
-          "python -u -m el_system.orchestrator.main"
+          "el_system.orchestrator.main"
         ]
         env {
           name = "ENV"
@@ -48,6 +48,9 @@ resource "google_cloud_run_v2_job" "dev_pipeline_run" {
       timeout = "600s"
       containers {
         image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:testing"
+        args = [
+          "orchestrator.orchestrator.main"
+        ]
          env {
           name = "TRIGGERED_BY"
           value = "scheduler"
@@ -82,7 +85,7 @@ resource "google_cloud_run_v2_job" "dev_metadata_system_run" {
       containers {
         image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:testing"
         args = [
-          "python -u -m metadata_system.orchestrator.main"
+          "metadata_system.orchestrator.main"
         ]
          env {
           name = "ENV"
@@ -124,7 +127,7 @@ resource "google_cloud_run_v2_job" "prod_el_system_run" {
       max_retries = 2
       timeout = "600s"
       containers {
-        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:latest"
+        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/el-job:latest"
         env {
           name = "COINGECKO_API_KEY"
           value_source {
@@ -160,7 +163,7 @@ resource "google_cloud_run_v2_job" "prod_pipeline_run" {
       max_retries = 2
       timeout = "600s"
       containers {
-        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:latest"
+        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/pipeline_run:latest"
         env {
         name = "DB_URL"
         value_source {
@@ -196,7 +199,7 @@ resource "google_cloud_run_v2_job" "prod_metadata_system_run" {
       max_retries = 2
       timeout = "600s"
       containers {
-        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/platform-job:latest"
+        image = "asia-south1-docker.pkg.dev/instant-medium-491107-t6/market-analytics-platform-repository/metadata-job:latest"
          env {
           name = "ENV"
           value = "PROD"
