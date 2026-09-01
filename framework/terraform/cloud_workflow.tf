64913196c98604a2fb40967bd47eef2525438f3d
Mayur Reddy <allamayurreddy@gmail.com>
2026-09-01 11:01:42 +0000

    Refactor cloud workflow resource to improve naming consistency and update description

--- framework/terraform/cloud_workflow.tf
+++ framework/terraform/cloud_workflow.tf
@@ -2,17 +2,15 @@
 #DEV
 ################################################################################
 
-
-resource "google_workflows_workflow" "dev_framework_orchestrator" {
-  name = "dev-framework-orchestrator"
+resource "google_workflows_workflow" "dev_orchestrator_workflow" {
+  name = "dev-orchestrator-workflow"
   region = var.gcp_region
-  description = "Dev Workflow for Framework Orchestration"
+  description = "Development Workflow for Orchestration"
   service_account = "development-cloud-resource-860@instant-medium-491107-t6.iam.gserviceaccount.com"
 
   deletion_protection = false
 
   call_log_level = "LOG_ALL_CALLS"
 
-  source_contents = templatefile("${path.module}/workflow.yml", {})
+  source_contents = templatefile("${path.module}/dev_workflow.yml", {})
 }
-
