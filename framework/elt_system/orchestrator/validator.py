8ca1125f1a270bb2c59affc434103e815a6f068e
Mayur Reddy <allamayurreddy@gmail.com>
2026-09-08 17:07:14 +0000

    Fix formatting issues in Validator class for improved consistency

--- framework/elt_system/orchestrator/validator.py
+++ framework/elt_system/orchestrator/validator.py
@@ -1,8 +1,7 @@
-from loguru import logger as log
-
 from pathlib import Path
 
 import jsonschema_rs
+from loguru import logger as log
 
 from elt_system.exceptions.exceptions import EXCEPTION_DESCRIPTIONS
 
@@ -26,7 +25,7 @@ class Validator:
             base_uri = Path(self.schema_path).resolve().as_uri()
 
             validate_cfg = jsonschema_rs.validator_for(
-                schema=self.schema_cfg, base_uri=base_uri
+                schema=self.schema_cfg, base_uri=base_uri,
             )
 
             validate_cfg.validate(instance=self.job_cfg)
@@ -35,7 +34,7 @@ class Validator:
 
         except Exception as excp:
             log.error(
-                f"{EXCEPTION_DESCRIPTIONS.get(type(excp), 'Unexpected Error Occured')}"
+                f"{EXCEPTION_DESCRIPTIONS.get(type(excp), 'Unexpected Error Occured')}",
             )
 
             raise
