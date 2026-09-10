af39157728e63b61de32ff9029a68f2ad0463d34
Mayur Reddy <allamayurreddy@gmail.com>
2026-09-10 01:24:05 +0000

    Fix formatting issues in test_main.py by adding trailing commas for improved consistency

--- framework/elt_system/orchestrator/tests/dev/unit/test_main.py
+++ framework/elt_system/orchestrator/tests/dev/unit/test_main.py
@@ -13,7 +13,7 @@ def test_main_local_success(mock_JobCatalog, mock_Orchestrator, mock_tpe):
     mock_JobCatalog.return_value.job_catalog_run.return_value = "LOCAL"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
@@ -48,7 +48,7 @@ def test_main_local_success(mock_JobCatalog, mock_Orchestrator, mock_tpe):
 def test_main_local_load_job_catalog_failed(mock_JobCatalog, mock_log):
 
     mock_JobCatalog.return_value.job_catalog_run.side_effect = Exception(
-        "System: elt | Failed to Load Job Catalog, Aborting Job Executions"
+        "System: elt | Failed to Load Job Catalog, Aborting Job Executions",
     )
 
     main = Main()
@@ -64,7 +64,7 @@ def test_main_local_load_job_catalog_failed(mock_JobCatalog, mock_log):
     mock_JobCatalog.return_value.job_catalog_run.assert_called_once()
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | Failed to Load Job Catalog, Aborting Job Executions"
+        "System: elt | Failed to Load Job Catalog, Aborting Job Executions",
     )
 
 
@@ -73,19 +73,19 @@ def test_main_local_load_job_catalog_failed(mock_JobCatalog, mock_log):
 @patch(target="elt_system.orchestrator.main.Orchestrator")
 @patch(target="elt_system.orchestrator.main.JobCatalog")
 def test_main_local_start_tpe_failed(
-    mock_JobCatalog, mock_Orchestrator, mock_tpe, mock_log
+    mock_JobCatalog, mock_Orchestrator, mock_tpe, mock_log,
 ):
 
     mock_JobCatalog.return_value.job_catalog_run.return_value = "LOCAL"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
 
     mock_executor.submit.side_effect = Exception(
-        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions"
+        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions",
     )
 
     main = Main()
@@ -111,7 +111,7 @@ def test_main_local_start_tpe_failed(
     assert mock_executor.submit.call_count == 1
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions"
+        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions",
     )
 
 
@@ -124,7 +124,7 @@ def test_main_local_job_failed(mock_log, mock_tpe, mock_Orchestrator, mock_JobCa
     mock_JobCatalog.return_value.job_catalog_run.return_value = "LOCAL"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
@@ -155,7 +155,7 @@ def test_main_local_job_failed(mock_log, mock_tpe, mock_Orchestrator, mock_JobCa
     assert mock_executor.submit.call_count == 1
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | One or More Jobs Failed"
+        "System: elt | One or More Jobs Failed",
     )
 
 
@@ -167,7 +167,7 @@ def test_main_success(mock_JobCatalog, mock_Orchestrator, mock_tpe):
     mock_JobCatalog.return_value.job_catalog_run.return_value = "DEV"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
@@ -189,7 +189,7 @@ def test_main_success(mock_JobCatalog, mock_Orchestrator, mock_tpe):
     mock_Orchestrator.assert_called_once_with(env="DEV")
 
     mock_executor.submit.assert_called_once_with(
-        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name"
+        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name",
     )
 
     assert mock_executor.submit.call_count == 1
@@ -200,7 +200,7 @@ def test_main_success(mock_JobCatalog, mock_Orchestrator, mock_tpe):
 def test_main_load_job_catalog_failed(mock_JobCatalog, mock_log):
 
     mock_JobCatalog.return_value.job_catalog_run.side_effect = Exception(
-        "System: elt | Failed to Load Job Catalog, Aborting Job Executions"
+        "System: elt | Failed to Load Job Catalog, Aborting Job Executions",
     )
 
     main = Main()
@@ -216,7 +216,7 @@ def test_main_load_job_catalog_failed(mock_JobCatalog, mock_log):
     mock_JobCatalog.return_value.job_catalog_run.assert_called_once()
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | Failed to Load Job Catalog, Aborting Job Executions"
+        "System: elt | Failed to Load Job Catalog, Aborting Job Executions",
     )
 
 
@@ -229,13 +229,13 @@ def test_main_start_tpe_failed(mock_JobCatalog, mock_Orchestrator, mock_tpe, moc
     mock_JobCatalog.return_value.job_catalog_run.return_value = "DEV"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
 
     mock_executor.submit.side_effect = Exception(
-        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions"
+        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions",
     )
 
     main = Main()
@@ -253,13 +253,13 @@ def test_main_start_tpe_failed(mock_JobCatalog, mock_Orchestrator, mock_tpe, moc
     mock_Orchestrator.assert_called_once_with(env="DEV")
 
     mock_executor.submit.assert_called_once_with(
-        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name"
+        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name",
     )
 
     assert mock_executor.submit.call_count == 1
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions"
+        "System: elt | Failed to Start the Thread Pool Executor, Aborting Job Executions",
     )
 
 
@@ -272,7 +272,7 @@ def test_main_job_failed(mock_log, mock_tpe, mock_Orchestrator, mock_JobCatalog)
     mock_JobCatalog.return_value.job_catalog_run.return_value = "DEV"
 
     mock_JobCatalog.return_value.jobs = [
-        {"path": "mock_path", "job_name": "mock_job_name"}
+        {"path": "mock_path", "job_name": "mock_job_name"},
     ]
 
     mock_executor = mock_tpe.return_value.__enter__.return_value
@@ -295,11 +295,11 @@ def test_main_job_failed(mock_log, mock_tpe, mock_Orchestrator, mock_JobCatalog)
     mock_Orchestrator.assert_called_once_with(env="DEV")
 
     mock_executor.submit.assert_called_once_with(
-        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name"
+        mock_Orchestrator.return_value.run_concurrent_jobs, "mock_path", "mock_job_name",
     )
 
     assert mock_executor.submit.call_count == 1
 
     mock_log.opt.return_value.critical.assert_called_once_with(
-        "System: elt | One or More Jobs Failed"
+        "System: elt | One or More Jobs Failed",
     )
