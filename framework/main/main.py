807c9c4e424fd28700aa3a66586f88e4645210e0
Mayur Reddy <allamayurreddy@gmail.com>
2026-09-14 12:52:32 +0000

    Refactor pipeline metadata endpoints for UUID-based run tracking

--- framework/main/main.py
+++ framework/main/main.py
@@ -2,11 +2,12 @@ import os
 import sys
 from datetime import datetime as dt
 from datetime import timezone as tz
+from uuid import UUID
 
-import psycopg2 as psg2
+import psycopg2 as pg2
 from fastapi import APIRouter as apir
 from loguru import logger as log
-from psycopg2.extras import Json
+from psycopg2.extras import register_uuid
 from uuid6 import uuid7 as uid
 
 log.remove()
@@ -22,140 +23,111 @@ log.add(
 log.add(sink=sys.stderr, filter=lambda record: record["level"].name == "CRITICAL")
 
 
-def pipeline_end_logger(self, payload: dict) -> None:
+##### Helper Functions :3 #####
 
-    pass
+def build_run_id() -> UUID:
 
+    runID: UUID = UUID(str(uid()))
 
-def job_logger(self, payload: dict) -> None:
+    log.info("Pipeline Run ID Created...")
 
-    with psg2.connect(payload["NeonDBURLSecret"]) as conn, conn.cursor() as cursor:
-        insert_query = """
-                INSERT INTO job_runs(
-                    run_id,
-                    pipeline_run_id,
-                    job_name,
-                    system,
-                    job_type,
-                    sub_jobtype,
-                    status,
-                    start_time,
-                    end_time,
-                    created_at,
-                    error_message,
-                    job_metrics,
-                    extra_metadata
-                )
+    return runID
 
-                VALUES(
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-                )
-            """
+def write_to_db(query: str, values: tuple, auth: str) -> None:
 
-        query_values: tuple = (
-            payload["job_start_metadata_result"]["job_run_id"],
-            payload["job_start_metadata_result"]["pipeline_run_id"],
-            payload["job_start_metadata_result"]["job_name"],
-            payload["job_start_metadata_result"]["system"],
-            payload["job_start_metadata_result"]["job_type"],
-            payload["job_start_metadata_result"]["sub_jobtype"],
-            payload["job_start_metadata_result"]["job_status"],
-            payload["job_start_metadata_result"]["job_start_time"],
-            payload["job_start_metadata_result"]["job_end_time"],
-            payload["job_start_metadata_result"]["job_created_at"],
-            payload["job_start_metadata_result"]["job_error_message"],
-            Json(payload["pipeline_start_metadata_result"]["job_metrics"]),
-            Json(payload["pipeline_start_metadata_result"]["extra_metadata"]),
-        )
+    register_uuid()
 
-        cursor.execute(insert_query, query_values)
+    with pg2.connect(str(auth)) as conn, conn.cursor() as csr:
+        csr.execute(query, values)
 
-    log.info("Successfully logged job start metadata into DB")
+    log.info("Successfully logged pipeline metadata into DB")
 
-
-def pipeline_end_metadata(self, payload: dict) -> dict:
-
-    pipeline_end_metadata_payload: dict = {
-        "pipeline_run_id": payload["pipeline_start_metadata_result"]["pipeline_run_id"],
-        "pipeline_name": payload["pipeline_start_metadata_result"]["pipeline_name"],
-        "pipeline_status": payload["pipeline_status"],
-        "pipeline_end_time": dt.now(tz=tz.utc).isoformat(sep="|"),
-        "pipeline_error_message": payload["pipeline_error_message"],
-        "job_counts": {
-            "total_jobs": payload["job_counts"]["total_jobs"],
-            "failed_jobs": payload["job_counts"]["failed_jobs"],
-            "successful_jobs": payload["job_counts"]["successful_jobs"],
-        },
-    }
-
-    log.info("Successfully created pipeline end metadata")
-
-    return pipeline_end_metadata_payload
+##### End #####
 
 
 router = apir()
 
-
-@router.post("/execute_jobs")
-async def execute_jobs(payload: dict) -> None:
-
-    pass
-
-
 @router.post("/pipeline_start_metadata")
 async def pipeline_start_metadata(payload: dict) -> dict:
 
+    runID = build_run_id()
+
     pipeline_start_metadata_payload: dict = {
-        "pipeline_run_id": str(uid()),
-        "pipeline_name": payload["pipeline_name"],
-        "pipeline_status": "RUNNING",
-        "pipeline_created_at": dt.now(tz=tz.utc),
-        "pipeline_start_time": dt.now(tz=tz.utc),
-        "pipeline_end_time": None,
-        "triggered_by": str(os.getenv(key="triggeredBy")) or "manual",
-        "pipeline_error_message": None,
-        "job_counts": {
-            "total_jobs": None,
-            "failed_jobs": None,
-            "successful_jobs": None,
-        },
+        "run_id": runID,
+        "pipeline_name": str(payload["pipeline_name"]),
+        "status": "RUNNING",
+        "created_at": dt.now(tz=tz.utc),
+        "start_time": dt.now(tz=tz.utc),
+        "end_time": None,
+        "triggered_by": str(os.environ["triggeredBy"]) or "manual",
+        "error_message": None,
     }
 
     log.info("Successfully created pipeline start metadata")
 
-    with psg2.connect(payload["neonDBURLSecret"]) as conn, conn.cursor() as cursor:
-        insert_query = """
-                INSERT INTO public.pipeline_runs(
-                    run_id,
-                    pipeline_name,
-                    status,
-                    start_time,
-                    end_time,
-                    created_at,
-                    triggered_by,
-                    error_message,
-                    job_counts
-                )
+    insert_query: str = """
+            INSERT INTO public.pipeline_runs(
+                run_id,
+                pipeline_name,
+                status,
+                created_at,
+                start_time,
+                end_time,
+                triggered_by,
+                error_message
+            )
 
-                VALUES(
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s
-                )
-            """
+            VALUES(
+                %s, %s, %s, %s, %s, %s, %s, %s
+            )
+        """
 
-        query_values: tuple = (
-            pipeline_start_metadata_payload["pipeline_run_id"],
-            pipeline_start_metadata_payload["pipeline_name"],
-            pipeline_start_metadata_payload["pipeline_status"],
-            pipeline_start_metadata_payload["pipeline_start_time"],
-            pipeline_start_metadata_payload["pipeline_end_time"],
-            pipeline_start_metadata_payload["pipeline_created_at"],
-            pipeline_start_metadata_payload["triggered_by"],
-            pipeline_start_metadata_payload["pipeline_error_message"],
-            Json(pipeline_start_metadata_payload["job_counts"]),
-        )
+    insert_query_values: tuple = (
+        pipeline_start_metadata_payload["run_id"],
+        pipeline_start_metadata_payload["pipeline_name"],
+        pipeline_start_metadata_payload["status"],
+        pipeline_start_metadata_payload["created_at"],
+        pipeline_start_metadata_payload["start_time"],
+        pipeline_start_metadata_payload["end_time"],
+        pipeline_start_metadata_payload["triggered_by"],
+        pipeline_start_metadata_payload["error_message"],
+    )
 
-        cursor.execute(insert_query, query_values)
-
-    log.info("Successfully logged pipeline start metadata into DB")
+    write_to_db(query=insert_query, values=insert_query_values, auth=payload["neonDBURL"])
 
     return pipeline_start_metadata_payload
+
+
+@router.post("/pipeline_end_metadata")
+async def pipeline_end_metadata(payload: dict) -> None:
+
+    pipeline_end_metadata_payload: dict = {
+        "run_id": payload["pipeline_start_metadata_payload"]["run_id"],
+        "pipeline_name": payload["pipeline_start_metadata_payload"]["pipeline_name"],
+        "status": payload["status"],
+        "end_time": dt.now(tz=tz.utc),
+        "error_message": payload.get("error_message") or None,
+    }
+
+    log.info("Successfully created pipeline start metadata")
+
+    update_query: str = """
+            UPDATE public.pipeline_runs
+            SET status=%s,
+                end_time=%s,
+                error_message=%s
+            WHERE run_id=%s
+                AND pipeline_name=%s
+        """
+
+    update_query_values: tuple = (
+        pipeline_end_metadata_payload["status"],
+        pipeline_end_metadata_payload["end_time"],
+        pipeline_end_metadata_payload["error_message"],
+        pipeline_end_metadata_payload["run_id"],
+        pipeline_end_metadata_payload["pipeline_name"],
+    )
+
+    write_to_db(query=update_query, values=update_query_values, auth=payload["neonDBURL"])
+
+    log.info("Successfully logged pipeline start metadata into DB")
