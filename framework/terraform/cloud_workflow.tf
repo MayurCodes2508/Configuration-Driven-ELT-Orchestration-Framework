c0629957653b2692846adc0f630eebefba30d4f9
Mayur Reddy <allamayurreddy@gmail.com>
2026-09-14 16:30:02 +0000

    Add production orchestration workflow resource

--- framework/terraform/cloud_workflow.tf
+++ framework/terraform/cloud_workflow.tf
@@ -14,3 +14,25 @@ resource "google_workflows_workflow" "dev_orchestrator_workflow" {
 
   source_contents = templatefile("${path.module}/dev_workflow.yml", {})
 }
+
+
+################################################################################
+#PROD
+################################################################################
+
+resource "google_workflows_workflow" "prod_orchestrator_workflow" {
+  name = "prod-orchestrator-workflow"
+  region = var.gcp_region
+  description = "Production Workflow for Orchestration"
+  service_account = "production-workflow@instant-medium-491107-t6.iam.gserviceaccount.com"
+
+  deletion_protection = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  call_log_level = "LOG_ALL_CALLS"
+
+  source_contents = templatefile("${path.module}/prod_workflow.yml", {})
+}
